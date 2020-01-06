d30d468edb11c87f17870acc26289b66815c1600
scottie-schneider <scott@laclave.co>
2020-01-06 17:35:55 -0500

    process last item

--- index.js
+++ index.js
@@ -68,8 +68,10 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+
+function processLastItem(stringList, callback) {
   /* CODE HERE */
+  return callback(stringList[stringList.length - 1])
 }
 
 /**
