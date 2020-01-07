a1313d490f2e1650128151db6e19b0bef0473576
scottie-schneider <scott@laclave.co>
2020-01-06 19:56:11 -0500

    all test passing

--- index.js
+++ index.js
@@ -314,8 +314,16 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(maxValue) {
+  let count = -1;
+  return function counter() {
+    if(count === maxValue){
+      return count = 0;
+    } else {
+      count++
+      return count
+    }
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
